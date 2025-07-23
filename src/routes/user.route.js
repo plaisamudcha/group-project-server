9727c90f38aa57d3b64b2ae004b08a94fc4b5dd4
SianCS <sian.mithuna@hotmail.co.th>
2025-07-23 10:22:04 +0700

    userroute refact

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,9 +4,11 @@ import userController from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
-userRoute.get("/", authMiddleware.checkToken,authMiddleware.isRole(["HR"]), userController.getAllUsers);
-userRoute.get("/:id", authMiddleware.checkToken, userController.getUserById);
-userRoute.post("/", authMiddleware.checkToken, authMiddleware.isRole(["HR"]), userController.createUser);
-userRoute.put("/:id", authMiddleware.checkToken, authMiddleware.isRole(["HR"]), userController.updateUser );
+userRoute.use(authMiddleware.checkToken);
+
+userRoute.get("/", authMiddleware.isRole(["HR"]), userController.getAllUsers);
+userRoute.get("/:id", userController.getUserById);
+userRoute.post("/", authMiddleware.isRole(["HR"]), userController.createUser);
+userRoute.put("/:id", authMiddleware.isRole(["HR"]), userController.updateUser);
 
 export default userRoute;
