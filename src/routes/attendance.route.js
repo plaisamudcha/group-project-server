52246a4fe2e33d15db620ad98eb34471be912a77
chitanupong <chitanupongss@hotmail.com>
2025-07-23 13:12:56 +0700

    add auten

--- src/routes/attendance.route.js
+++ src/routes/attendance.route.js
@@ -11,14 +11,14 @@
 //*************************************************** */
 
 import express from "express";
-import authMiddleware from "../middlewares/auth.middleware.js"
+import authMiddleware from '../middlewares/auth.middleware.js';
 import attendanceController from "../controllers/attendance.controller.js";
 
 const attendanceRoute = express.Router();
 
-attendanceRoute.post("/clock-in", authMiddleware, attendanceController.clockIn);
-attendanceRoute.post("/clock-out", authMiddleware, attendanceController.clockOut);
-attendanceRoute.get("/", authMiddleware, attendanceController.getAttendanceList);
-attendanceRoute.get("/reports", authMiddleware, attendanceController.getReports);
+attendanceRoute.post("/clock-in", authMiddleware.checkToken, attendanceController.clockIn);
+attendanceRoute.post("/clock-out", authMiddleware.checkToken, attendanceController.clockOut);
+attendanceRoute.get("/", authMiddleware.checkToken, attendanceController.getAttendanceList);
+attendanceRoute.get("/reports", authMiddleware.checkToken, attendanceController.getReports);
 
 export default attendanceRoute;
