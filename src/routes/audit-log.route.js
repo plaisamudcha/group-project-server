4c04414b9bf4c8d0413fd1e74f73ac818bbd65e3
terachot-git <terachotdev@gmail.com>
2025-07-23 10:57:28 +0700

    first version

--- src/routes/audit-log.route.js
+++ src/routes/audit-log.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authMiddleware from "../middlewares/auth.middleware";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const auditRoute = express.Router();
 
--- src/routes/leave.route.js
+++ src/routes/leave.route.js
@@ -1,10 +1,13 @@
 import express from "express";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import leaveController from "../controllers/leave.controller.js";
 
 const leaveRoute = express.Router();
-
+leaveRoute.use(authMiddleware.checkToken)
 leaveRoute.post("/", () => {});
-leaveRoute.get("/", () => {});
-leaveRoute.get("/:id", () => {});
+leaveRoute.get("/",authMiddleware.isRole('hr'),leaveController.getAllLeaves);
+leaveRoute.get("/user", leaveController.getUserLeaves);
+leaveRoute.get("/:id", leaveController.getLeavesDetails);
 leaveRoute.put("/:id/status", () => {});
 
 export default leaveRoute;
