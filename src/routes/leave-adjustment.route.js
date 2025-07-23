6678fde30468b596107d0d6e82f09a80930a6964
plaisamudcha <plai_samudcha@hotmail.com>
2025-07-23 09:55:48 +0700

    finish feature leaveadjustment

--- src/routes/leave-adjustment.route.js
+++ src/routes/leave-adjustment.route.js
@@ -1,8 +1,13 @@
 import express from "express";
+import shiftController from "../controllers/shift.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const leaveAdjustmentRoute = express.Router();
 
-leaveAdjustmentRoute.get("/", () => {});
-leaveAdjustmentRoute.post("/", () => {});
+leaveAdjustmentRoute.use(authMiddleware.checkToken);
+
+leaveAdjustmentRoute.get("/", shiftController.getAllshift);
+leaveAdjustmentRoute.post("/", shiftController.createShift);
+leaveAdjustmentRoute.put("/:id", shiftController.updateShift);
 
 export default leaveAdjustmentRoute;
