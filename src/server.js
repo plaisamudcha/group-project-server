bacaaa72df72ad2e41253f3362e739bedcee0fbe
plaisamudcha <plai_samudcha@hotmail.com>
2025-07-28 10:24:48 +0700

    add node-cron

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import shutdown from "./utils/shutdown.util.js";
+import runAttendanceCron from "./node-cron/attendance.node-cron.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  runAttendanceCron();
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 process.on("SIGINT", () => shutdown("SIGINT"));
 process.on("SIGTERM", () => shutdown("SIGTERM"));
