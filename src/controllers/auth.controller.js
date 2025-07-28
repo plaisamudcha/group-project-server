b8ca8cd2993afb3fe35e45ff2f7fcd90ace946de
plaisamudcha <plai_samudcha@hotmail.com>
2025-07-28 12:02:40 +0700

    add refresh table,refreshToken-feature,forgotpassword,resetpassword

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import authService from "../services/auth.service.js";
 import createError from "../utils/create-error.util.js";
 import genTokenJWT from "../utils/jwt.util.js";
+import prisma from "../config/prisma.js";
+import sendResetEmail from "../utils/reset-email.util.js";
 
 const authController = {
   register: async (req, res) => {
@@ -27,12 +29,70 @@ const authController = {
     };
 
     const assessToken = genTokenJWT.loginToken(payload);
-
-    const { password: hashedPassword, ...user } = foundUser;
-
-    res.json({ message: "เข้าสู่ระบบสำเร็จ", assessToken, user });
+    const refreshToken = genTokenJWT.refreshToken(payload);
+    await prisma.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: foundUser.id,
+        expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+      },
+    });
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+    res.json({ message: "เข้าสู่ระบบสำเร็จ", assessToken, user: foundUser });
   },
-
+  forgotPassword: async (req, res) => {
+    const { email } = req.body;
+    const user = await authService.findExistUser(email);
+    if (!user) {
+      createError(400, "ไม่พบผู้ใช้งาน");
+    }
+    const payload = { id: user.id, role: user.role };
+    const resetToken = genTokenJWT.forgotPasswordToken(payload);
+    await sendResetEmail(user, resetToken);
+    res.json({ message: "กรุณาตรวจสอบอีเมลของคุณเพื่อรีเซ็ตรหัสผ่าน" });
+  },
+  resetPassword: async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+    const payload = genTokenJWT.checkResetPasswordToken(token);
+    const user = await authService.findId(payload.id);
+    if (!user) {
+      createError(400, "ไม่พบผู้ใช้งาน");
+    }
+    await authService.resetPassword(user.id, password);
+    res.json({ message: "รีเซ็ตรหัสผ่านสำเร็จ" });
+  },
+  refreshToken: async (req, res) => {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      createError(401, "กรุณาเข้าสู่ระบบ");
+    }
+    const foundToken = await authService.findRefreshToken(refreshToken);
+    if (!foundToken) {
+      createError(401, "กรุณาเข้าสู่ระบบ");
+    }
+    let userId = foundToken.userId;
+    let role = foundToken.user.role;
+    if (new Date() > foundToken.expiresAt) {
+      await authService.deleteRefreshToken(refreshToken);
+      createError(401, "กรุณาเข้าสู่ระบบ");
+    }
+    const newrefreshToken = genTokenJWT.refreshToken({ id: userId, role });
+    const newAssessToken = genTokenJWT.loginToken({ id: userId, role });
+    await authService.updateRefreshToken(newrefreshToken, userId);
+    res.cookie("refreshToken", newrefreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+    res.json({ message: "รีเฟรชโทเค็นสำเร็จ", newAssessToken });
+  },
   getMe: async (req, res) => {
     const user = await authService.findId(req.user.id);
     if (!user) {
