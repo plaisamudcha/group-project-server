9e806dbf04e16c2d5c4432b8710d1709dd187ce8
Thanaton2002 <thanaton.ims@gmail.com>
2025-08-04 10:53:45 +0700

    validate mini fix

--- src/services/audit-log.service.js
+++ src/services/audit-log.service.js
@@ -2,7 +2,19 @@ import prisma from "../config/prisma.js";
 
 const auditService = {
     fetchAuditLog: () => {
-        return prisma.auditLog.findMany()
+        return prisma.auditLog.findMany({
+            include: {
+                user: {  
+                    select: {
+                        id: true,
+                        name: true,
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'  
+            }
+        })
     },
     createAuditLog: (data,tx = prisma) => {
         return tx.auditLog.create({data})
